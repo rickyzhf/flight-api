38d10a01a017372f957a5edbd984ccf293186fca
qingfengonline@hotmail.com <qingfengonline@hotmail.com>
2025-06-07 08:51:39 +0800

    id

--- src/main/java/com/example/flightapi/controller/FlightController.java
+++ src/main/java/com/example/flightapi/controller/FlightController.java
@@ -1,14 +1,24 @@
 package com.example.flightapi.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.flightapi.dto.BaseResponse;
 import com.example.flightapi.dto.FlightSearchDTO;
 import com.example.flightapi.entity.Flight;
 import com.example.flightapi.service.FlightService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -22,5 +32,17 @@ public class FlightController {
     public ResponseEntity<BaseResponse<List<Flight>>> searchFlights(@RequestBody FlightSearchDTO searchDTO) {
         List<Flight> flights = flightService.searchFlights(searchDTO);
         return ResponseEntity.ok(BaseResponse.success(flights));
+    }
+
+    @GetMapping("/searchId")
+    public ResponseEntity<BaseResponse<Flight>> searchFlightsId(@RequestParam Long id) {
+    	Optional<Flight> flights = flightService.searchFlightsId(id);
+//        return ResponseEntity.ok(BaseResponse.success(flights));
+        if (flights.isPresent()) {
+            return ResponseEntity.ok(BaseResponse.success(flights.get()));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(BaseResponse.error(404,"Flight not found"));
+        }
     }
 }
